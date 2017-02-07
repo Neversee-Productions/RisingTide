ead05474a8290fc3200d16d46aa93daabc93b85d
rafael-girao <c00203250@itcarlow.ie>
2017-02-07 23:08:57 +0000

    Implemented Release Version
    Adjusted library linking to appropriate release versions

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,3 @@
-#ifdef _DEBUG 
-#pragma comment(lib,"sfml-graphics-d.lib") 
-#pragma comment(lib,"sfml-audio-d.lib") 
-#pragma comment(lib,"sfml-system-d.lib") 
-#pragma comment(lib,"sfml-window-d.lib") 
-#pragma comment(lib,"sfml-main-d.lib")
-#pragma comment(lib,"sfml-network-d.lib")
-#pragma comment(lib, "thor-d.lib")
-#else 
-#pragma comment(lib,"sfml-graphics.lib") 
-#pragma comment(lib,"sfml-audio.lib") 
-#pragma comment(lib,"sfml-system.lib") 
-#pragma comment(lib,"sfml-main.lib")
-#pragma comment(lib,"sfml-window.lib") 
-#pragma comment(lib,"sfml-network.lib") 
-#pragma comment(lib, "thor.lib")
-#endif 
-#pragma comment(lib,"opengl32.lib")
-#pragma comment(lib,"glu32.lib")
-
-
 // includes
 #include "Game.h"
 
